8a4bc5a43709da28d17b9760df820cb4fc901a00
Wim De Meester <wim.demeester@ster.kuleuven.be>
2017-01-19 15:33:34 +0100

    Check out the correct branch.

--- devel/99Build.sh
+++ devel/99Build.sh
@@ -2,14 +2,14 @@
 ./00SetupAnaconda.sh
 
 # Check out the conda-devel branch with a list of all conda packages to install
-git clone https://github.com/IvS-KULeuven/MIRICLE2.git
+git clone -b conda-devel https://github.com/IvS-KULeuven/MIRICLE2.git
 
-git checkout conda-devel
-
+ls
 
 mkdir -p /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}
-cp miricle-*-py27.0.txt /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}
+cp MIRICLE2/miricle-*-py27.0.txt /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}
 
+ls
 
 echo ${BUILD_NUMBER} >> /srv/www/www.miricle.org/MIRICLE2/devel/buildNumbers
 
