97ee483d666c36f28807d0fe0dcb0775bcfa9ac1
Wim De Meester <wim.demeester@ster.kuleuven.be>
2017-01-24 16:08:02 +0100

    Added cr-sim-ramp-fits

---
+++ devel/02CrSimRampFits.sh
@@ -0,0 +1,36 @@
+rm -rf jenkins
+git clone -b jenkins https://github.com/IvS-KULeuven/MIRICLE2.git jenkins
+
+rm -rf conda-devel
+git clone -b conda-devel https://github.com/IvS-KULeuven/MIRICLE2.git conda-devel
+mv conda-devel/miricle-linux-py27.0.txt .
+
+bash jenkins/devel/01TestDevelCondaPackageInstallation.sh
+
+export PATH=/export/disk/anaconda.42/bin:$PATH
+
+source activate miricle.devel
+
+rm -rf cr-sim-ramp-fits
+mkdir cr-sim-ramp-fits
+cd cr-sim-ramp-fits
+touch meta.yaml
+echo "package:" > meta.yaml
+echo "  name: cr-sim-ramp-fits" >> meta.yaml
+version=`grep version ../defsetup.py | sed "s/     'version' : '//g"  | sed "s/',//g"`
+echo "  version: \"$version\"" >> meta.yaml
+echo "" >> meta.yaml
+echo "source:" >> meta.yaml
+echo "  url: https://aeon.stsci.edu/ssb/svn/jwst/trunk/prototypes/cr_sim_ramp_fit" >> meta.yaml
+echo "" >> meta.yaml
+echo "requirements:" >> meta.yaml
+echo "  build:" >> meta.yaml
+echo "    - python" >> meta.yaml
+echo "  run:" >> meta.yaml
+echo "    - python" >> meta.yaml
+
+cd ..
+conda build cr-sim-ramp-fits --output-folder=/tmp/
+
+conda install /tmp/linux-64/cr-sim-ramp-fits-$version-py27_0.tar.bz2
+conda build purge
