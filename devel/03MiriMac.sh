f9b8b561c7b921ea467d723130ed1c49a53238d7
Wim De Meester <wim.demeester@ster.kuleuven.be>
2017-01-25 15:26:15 +0100

    Add build scripts for miri

--- devel/03MiriMac.sh
+++ devel/03MiriMac.sh
@@ -3,25 +3,25 @@ git clone -b jenkins https://github.com/IvS-KULeuven/MIRICLE2.git jenkins
 
 rm -rf conda-devel
 git clone -b conda-devel https://github.com/IvS-KULeuven/MIRICLE2.git conda-devel
-mv conda-devel/miricle-linux-py27.0.txt .
+mv conda-devel/miricle-osx-py27.0.txt .
 
-bash jenkins/devel/01TestDevelCondaPackageInstallation.sh
+bash jenkins/devel/01TestDevelCondaPackageInstallationMac.sh
 
-export PATH=/export/disk/anaconda.42/bin:$PATH
+export PATH=/Users/jenkins/anaconda2/bin:$PATH
 
 source activate miricle.devel
 
-rm -rf cr-sim-ramp-fit
-mkdir cr-sim-ramp-fit
-cd cr-sim-ramp-fit
+rm -rf miri
+mkdir miri
+cd miri
 touch meta.yaml
 echo "package:" > meta.yaml
-echo "  name: cr-sim-ramp-fit" >> meta.yaml
-version=`grep version ../defsetup.py | sed "s/     'version' : '//g"  | sed "s/',//g"`
+echo "  name: miri" >> meta.yaml
+version=`grep version ../lib/__init__.py | sed "s/__version__ = '//g"  | sed "s/'//g"`
 echo "  version: \"$version\"" >> meta.yaml
 echo "" >> meta.yaml
 echo "source:" >> meta.yaml
-echo "  url: https://aeon.stsci.edu/ssb/svn/jwst/trunk/prototypes/cr_sim_ramp_fit" >> meta.yaml
+echo "  url: https://aeon.stsci.edu/ssb/svn/jwst/trunk/teams/miri" >> meta.yaml
 echo "" >> meta.yaml
 echo "requirements:" >> meta.yaml
 echo "  build:" >> meta.yaml
@@ -30,7 +30,8 @@ echo "  run:" >> meta.yaml
 echo "    - python" >> meta.yaml
 
 cd ..
-conda build cr-sim-ramp-fit --output-folder=/tmp/
+rm -rf /tmp/osx-64
+conda build miri --output-folder=/tmp/
 
-conda install /tmp/linux-64/cr-sim-ramp-fit-$version-py27_0.tar.bz2
+conda install /tmp/osx-64/miri-$version-py27_0.tar.bz2
 conda build purge
