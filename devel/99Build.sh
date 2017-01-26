9055f1b99d6d1c3e6cd482f16ff2dc61044372cd
Wim De Meester <wim.demeester@ster.kuleuven.be>
2017-01-26 15:24:55 +0100

    Save the correct version to miricle-xxx-py27.0.txt

--- devel/99Build.sh
+++ devel/99Build.sh
@@ -16,9 +16,11 @@ cd cr_sim_ramp_fit
 mv ../02CrSimRampFit.sh .
 ./02CrSimRampFit.sh /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}
 
+file=`ls /srw/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/linux-64/ | grep cr-sim-ramp-fit`
+
 # Add cr-sim-ramp-fit to the install files
-echo "http://www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/linux-64/cr-sim-ramp-fit-$version-py27_0.tar.bz2" >> /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/miricle-linux-py27.0.txt
-echo "http://www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/osx-64/cr-sim-ramp-fit-$version-py27_0.tar.bz2" >> /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/miricle-osx-py27.0.txt
+echo "http://www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/linux-64/$file" >> /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/miricle-linux-py27.0.txt
+echo "http://www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/osx-64/$file" >> /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/miricle-osx-py27.0.txt
 
 cd ..
 rm -rf miri
@@ -26,8 +28,11 @@ svn checkout https://aeon.stsci.edu/ssb/svn/jwst/trunk/teams/miri
 cd miri
 mv ../03Miri.sh .
 ./03Miri.sh /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}
-echo "http://www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/linux-64/miri-$version-py27_0.tar.bz2" >> /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/miricle-linux-py27.0.txt
-echo "http://www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/osx-64/miri-$version-py27_0.tar.bz2" >> /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/miricle-osx-py27.0.txt
+
+file=`ls /srw/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/linux-64/ | grep miri-`
+
+echo "http://www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/linux-64/$file" >> /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/miricle-linux-py27.0.txt
+echo "http://www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/osx-64/$file" >> /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}/miricle-osx-py27.0.txt
 
 # Copy the MAC installations to the correct directory
 scp -r munki:/Users/jenkins/condaBuild/osx-64 /srv/www/www.miricle.org/MIRICLE2/devel/${BUILD_NUMBER}
